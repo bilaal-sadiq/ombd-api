bda70761d3e98328cd4799fb5b3b34ff819f3370
mrmbs1 <mbilalsadiq.19@gmail.com>
2024-07-16 22:41:11 +0100

    css layout change and api ey added

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // https://www.omdbapi.com/?i=tt3896198&apikey=ba7312e3
+
+// https://www.omdbapi.com/?s=tt3896198&apikey=ba7312e3
+
+// http://www.omdbapi.com/?apikey=[ba7312e3]&
+
+// http://img.omdbapi.com/?apikey=[ba7312e3]&
