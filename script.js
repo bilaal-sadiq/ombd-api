8fa1c7dfaccc5ccf8499f20e00cfbdac669ef89d
mrmbs1 <mbilalsadiq.19@gmail.com>
2024-07-17 16:31:51 +0100

    fetch api key

--- script.js
+++ script.js
@@ -5,3 +5,26 @@
 // http://www.omdbapi.com/?apikey=[ba7312e3]&
 
 // http://img.omdbapi.com/?apikey=[ba7312e3]&
+
+
+async function main() {
+    const movies = await fetch ("https://www.omdbapi.com/?i=tt3896198&apikey=ba7312e3")
+    const movieData = await movies.json();
+    const movieListEl = document.querySelector(".movie-card");
+
+
+    movieListEl.innerHTML =  
+
+    movieData.map((movie) => `<div class="movie-card">
+        <img src="./assets/medium-cover.jpg" alt="">
+        <h3>Movie Name</h3>
+        <p>Movie year</p>
+        </div>`
+    ).join("");
+
+        
+       
+}
+
+main();
+
